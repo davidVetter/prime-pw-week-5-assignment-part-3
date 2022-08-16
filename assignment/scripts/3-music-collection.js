db9e96d8ba0cfd1ca652a978fcfab86895a10393
David Vetter <ddvetter23@gmail.com>
2022-08-16 13:15:41 -0500

    added 6 albums to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -18,10 +18,11 @@ function addToCollection(title, artist, year) {
     return addObj;
 }
 
-console.log(addToCollection('test1', 'David', 2005));
-console.log(addToCollection('test2', 'Name2', 2010));
+console.log(addToCollection('Glass Houses', 'Billy Joel', 1980));
+console.log(addToCollection('52nd Street', 'Billy Joel', 1978));
+console.log(addToCollection("Ropin' The Wind", 'Garth Brooks', 1991));
+console.log(addToCollection('Maybe Memories', 'The Used', 2003));
+console.log(addToCollection('Sing The Sorrow', 'AFI', 2003));
+console.log(addToCollection('Swan Songs', 'Hollywood Undead', 2008));
 console.log('Collection after adding ', collection);
-console.log('The 2nd albums in my collection is ', collection[1]);
-collection[0].title = 'shouldChange' // This for testing, REMOVE FROM FINISHED PRODUCT
-console.log('I just changed the title of the 1st album ', collection[0]);
-console.log('My collection includes ', collection);
+console.log('The 3rd albums in my collection is ', collection[2]);
