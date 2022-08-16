33c4aab31cd0d37b5f31f85cdc91ce8a2a2337df
David Vetter <ddvetter23@gmail.com>
2022-08-16 11:41:41 -0500

    added working version of addToCollction function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,27 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+let albumNum = 0;
+
+function addToCollection(title, artist, year) {
+    let addTitle = title;
+    let addArtist = artist;
+    let pubYear = year;
+    const addObj = {
+        title: addTitle,
+        artist: addArtist,
+        year: pubYear,
+    }
+    console.log('This is what is being added', addObj);
+    console.log('Collection prior to push', collection);
+    collection.push(addObj);
+    return addObj;
+}
+
+console.log('Lets test adding an album!', addToCollection('test1', 'David', 2005));
+console.log('Lets test adding an album!', addToCollection('test2', 'Name2', 2010));
+console.log(collection);
+console.log(collection[1]);
+collection[0].title = 'shouldChange'
+console.log(collection[0]);
+console.log(collection);
