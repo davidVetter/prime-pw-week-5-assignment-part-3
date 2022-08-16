1fac7371a0f9df2d98d5b6e499da76b7eb79527d
David Vetter <ddvetter23@gmail.com>
2022-08-16 15:22:50 -0500

    added showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,28 +1,44 @@
 console.log('***** Music Collection *****')
 
-let collection = [];
-let albumNum = 0;
+let collection = []; // creating collection array
 
+// Function that accepts an album title, artist and year published as 
+// arguments and adds them to the array collection as objects
+// Returns object created
 function addToCollection(title, artist, year) {
-    let addTitle = title;
-    let addArtist = artist;
-    let pubYear = year;
-    const addObj = {
-        title: addTitle,
-        artist: addArtist,
-        year: pubYear,
+    let addTitle = title; // variable to for title argument
+    let addArtist = artist; // variable for artist argument
+    let pubYear = year; // variable for year argument
+    const addObj = { // create new object
+        title: addTitle, // title of album property
+        artist: addArtist, // artist property
+        year: pubYear, // year published property
     }
-    console.log('This is what is being added', addObj);
-    console.log('Collection prior to push', collection);
-    collection.push(addObj);
-    return addObj;
-}
+    //console.log('This is what is being added', addObj); // tracking function logs
+    //console.log('Collection prior to push', collection);
+    collection.push(addObj); // add new object to array collection
+    return addObj; // return new object
+} // end addToCollection function
 
-console.log(addToCollection('Glass Houses', 'Billy Joel', 1980));
+// Test data for add to collection, adds 6 albums, logs collection after adding
 console.log(addToCollection('52nd Street', 'Billy Joel', 1978));
+console.log(addToCollection('Glass Houses', 'Billy Joel', 1980));
 console.log(addToCollection("Ropin' The Wind", 'Garth Brooks', 1991));
 console.log(addToCollection('Maybe Memories', 'The Used', 2003));
 console.log(addToCollection('Sing The Sorrow', 'AFI', 2003));
 console.log(addToCollection('Swan Songs', 'Hollywood Undead', 2008));
 console.log('Collection after adding ', collection);
 console.log('The 3rd albums in my collection is ', collection[2]);
+
+// Function that accepts an array and displays the number of objects 
+// and their contents in a readable format
+function showCollection(array) {
+    let arr = array // variable for the argument
+    console.log(`The number of items in this collection is ${arr.length}`); // logs number in given array
+    for (let record of arr) { // for loop to go through given array
+        console.log(`${record.title} by ${record.artist}, published in ${record.year}`); // foramtted log of each object
+    } // end for loop
+} // end showCollection function
+
+showCollection(collection); // call showCollection function with collection array as argument
+
